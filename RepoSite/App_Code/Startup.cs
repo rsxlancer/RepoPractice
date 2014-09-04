07263ab932d4fc83a2e6f96547f3dfb0b977eb85
rsxlancer <rsxlancer@gmail.com>
2014-09-04 09:08:57 -0600

    Initial Website and Solution

---
+++ RepoSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepoSite.Startup))]
+namespace RepoSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
